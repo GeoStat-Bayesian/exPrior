cd1fd1af7f02cfbc0264a6906d7784c48d66cf19
kcucchi <karina.cucchi@gmail.com>
2017-02-22 13:56:44 -0800

    fix library stuff

--- R/function_generalfromMeas.R
+++ R/function_generalfromMeas.R
@@ -20,43 +20,43 @@ generalFromMeas <- function(meas,
                             theta,
                             niter=10^5,
                             hierarchicalSigma=F){
-  
+
   #################################
   # define data from measurements #
   #################################
-  
+
   # meas is a list of measurements
   # transform to matrix
   I = length(meas)                       # number of sites
   J_i = unlist(lapply(meas,length))      # number of measurement per site
   nbMeasMax <- max(J_i)                  # maximum number of measurements at one site
-  
+
   # create empty matrix with all NA
   measMatrix <- array(data = NA,dim = c(I,nbMeasMax))
-  
+
   # fill matrix with values
   for(iList in 1:length(meas)){
     measMatrix[iList,1:length(meas[[iList]])] <- meas[[iList]]
   }
-  
+
   siteData <- list(theta = measMatrix)
-  
+
   #############################################
   # define hierarchical model using bugs code #
   #############################################
-  
+
   if(hierarchicalSigma){ # if sigma is defined using a hierarchical model
-    
+
     siteHierarchyCode <-
-      
+
       nimble::nimbleCode({
-        
+
         # prior distribution of hyperparameters
         alpha ~ dunif(min = -10,max = 10)
         tau ~ dunif(min = 0, max = 2)
         beta ~ dunif(min = 0, max = 5)
         xi ~ dunif(min = 0, max = 2)
-        
+
         # hierarchical model at each site
         for (i in 1:I){ # loop over sites
           mu[i] ~ dnorm(mean = alpha,sd = tau) # distribution of mean at site i
@@ -67,15 +67,15 @@ generalFromMeas <- function(meas,
             theta[i,j] ~ dnorm(mean = mu[i],sd = sqrt(sigma2[i]))
           }
         }
-        
+
       })
-    
+
   }else{
-    
+
     siteHierarchyCode <-
-      
+
       nimble::nimbleCode({
-        
+
         # prior distribution of hyperparameters
         # see http://www.stats.org.uk/priors/Priors.pdf for formulation of approximations
         # approximates flat prior
@@ -89,7 +89,7 @@ generalFromMeas <- function(meas,
         tau <- xiTau/sqrt(chSqTau)
         # approximates Jeffrey's prior (inverse prior)
         sigma ~ dgamma(shape = 0.0001, rate = 0.0001)
-        
+
         # hierarchical model at each site
         for (i in 1:I){ # loop over sites
           mu[i] ~ dnorm(mean = alpha,sd = tau) # distribution of mean at site i
@@ -98,13 +98,13 @@ generalFromMeas <- function(meas,
             theta[i,j] ~ dnorm(mean = mu[i],sd = sigma)
           }
         }
-        
+
       })
-    
+
   }
-  
+
   siteConst <- list(I = nrow(measMatrix),J=J_i)
-  
+
   if(hierarchicalSigma){
     siteInit <- list(alpha = 0,
                      tau = 1,
@@ -118,7 +118,7 @@ generalFromMeas <- function(meas,
                      tau = 1, # sd of means
                      mu = unlist(lapply(X = meas,FUN = mean,na.rm=T)))
   }
-  
+
   # actually create the model
   siteModel <-
     nimble::nimbleModel(code = siteHierarchyCode,
@@ -126,58 +126,58 @@ generalFromMeas <- function(meas,
                         data = siteData,
                         inits = siteInit,
                         name = 'siteModel')
-  
+
   # check that the model makes sense
   # siteModel$getNodeNames()
-  
+
   # compile the model
   CsiteModel <- nimble::compileNimble(siteModel)
-  
+
   ##########################
   # Configure and run MCMC #
   ##########################
-  
+
   # names of hyperparameters to monitor
   if(hierarchicalSigma){
     which_monitor = c('alpha', 'tau', 'beta', 'xi')
   }else{
     which_monitor = c('alpha', 'tau', 'sigma')
   }
-  
-  
+
+
   # create compile and run MCMC
   siteModelConf <- nimble::configureMCMC(model = siteModel,print=T)
-  
+
   siteModelConf$addMonitors(which_monitor)
-  
+
   siteModelMCMC <- nimble::buildMCMC(siteModelConf)
   CsiteModelMCMC <- nimble::compileNimble(siteModelMCMC, project = siteModel)
-  
+
   CsiteModelMCMC$run(niter)
   MCMCsamples <- as.matrix(CsiteModelMCMC$mvSamples)
-  
+
   # effectiveSize function from library coda
   MCMC_effectiveSizes = numeric(length(which_monitor))
   for(i in 1:length(MCMC_effectiveSizes)){
-    MCMC_effectiveSizes[i] = effectiveSize(MCMCsamples[ , which_monitor[i] ])
+    MCMC_effectiveSizes[i] = coda::effectiveSize(MCMCsamples[ , which_monitor[i] ])
   }
-  
+
   ###############################################################
   # Calculate prior and posterior densities for hyperparameters #
   ###############################################################
-  
+
   hyperPar = which_monitor
-  
+
   ## sample from prior
   d_hyperPar_prior = list()
-  
-  
+
+
   if(hierarchicalSigma){
-    
+
     # define hyperprior for alpha
     d_hyperPar_prior[[1]] <- data.frame(x=seq(from=-10,to=10,by=0.1))
     d_hyperPar_prior[[1]]$y <- dunif(d_hyperPar_prior[[1]]$x,min = -10,max = 10)
-    
+
     # define hyperprior for tau
     x_tau <- seq(from=0.001,to=2,by=0.001)
     y_tau <- dunif(x_tau,min = 0,max = 2)
@@ -186,33 +186,33 @@ generalFromMeas <- function(meas,
     # y_tau <- dgamma(x = x_tau,shape = 0.001,rate = 0.001)
     # pdf_tau_norm <- normalize_pdf(x = x_tau,p_x = y_tau)
     # d_hyperPar_prior[[2]] <- data.frame(x=pdf_tau_norm$x,y=pdf_tau_norm$p_x)
-    
+
     # define hyperprior for beta
     d_hyperPar_prior[[3]] <- data.frame(x=seq(from=-0.5,to=5.5,by=0.01))
     d_hyperPar_prior[[3]]$y <- dunif(d_hyperPar_prior[[3]]$x,min = 0, max = 5)
-    
+
     # define hyperprior for xi
     d_hyperPar_prior[[4]] <- data.frame(x=seq(from=-0.5,to=2.5,by=0.01))
     d_hyperPar_prior[[4]]$y <- dunif(d_hyperPar_prior[[4]]$x,min = 0, max = 2)
-    
+
   }else{
-    
+
     # define hyperprior for alpha
     d_hyperPar_prior[[1]] <- data.frame(x=seq(from=-10,to=10,by=0.1)) # define boundaries for the hyperprior
     d_hyperPar_prior[[1]]$y <- dnorm(x = d_hyperPar_prior[[1]]$x,mean = 0,sd = 1000)
-    
+
     # define hyperprior for tau
     d_hyperPar_prior[[2]] <- data.frame(x=seq(from=0.001,to=2,by=0.001)) # define boundaries for the hyperprior
     d_hyperPar_prior[[2]]$y <- dcauchy(x = d_hyperPar_prior[[2]]$x, location = 0,scale = 25)
-    
+
     # define hyperprior for sigma
     d_hyperPar_prior[[3]] <- data.frame(x=seq(from=0.001,to=2,by=0.001)) # define boundaries for the hyperprior
     d_hyperPar_prior[[3]]$y <- dgamma(x = d_hyperPar_prior[[3]]$x, shape = 0.0001, rate = 0.0001)
-    
+
   }
-  
+
   ## sample from posterior
-  
+
   d_hyperPar_post = list()
   for(i in 1:length(hyperPar)){
     # calculate density from MCMC samples using kernels
@@ -226,21 +226,21 @@ generalFromMeas <- function(meas,
                                    xout = d_hyperPar_prior[[i]]$x,
                                    yleft=0,yright=0) # define probability 0 outside density bounds
   }
-  
+
   #######################################################
   # Sample theta from posterior predictive distribution #
   #######################################################
-  
+
   # We can sample from the posterior predictive distribution by sampling from
   # each eta sample from the Monte-Carlo Markov Chain.
-  
+
   # draw one theta sample per eta value
   samp_theta_pred = numeric(length = 100*nrow(MCMCsamples))
   # loop over samples in MCMC
   for(iEta in 1:nrow(MCMCsamples)){
-    
+
     if(hierarchicalSigma){
-      
+
       # draw realizations of the site-specific parameters
       mu_s = rnorm(n = 10,
                    mean = MCMCsamples[iEta,'alpha'],
@@ -249,65 +249,65 @@ generalFromMeas <- function(meas,
                             shape = MCMCsamples[iEta,'beta'],
                             rate = MCMCsamples[iEta,'xi'])
       sigma2_s <- 1/inv_sigma2_s
-      
+
       # draw realizations of the physical parameter
       for(iPhi in 1:10){
-        samp_theta_pred[100*(iEta-1)+10*(iPhi-1)+(1:10)] <- 
+        samp_theta_pred[100*(iEta-1)+10*(iPhi-1)+(1:10)] <-
           rnorm(n = 10,
                 mean = mu_s[iPhi],
                 sd = sqrt(sigma2_s[iPhi]))
       }
-      
+
     }else{
-      
+
       # draw realizations of the mean at the new site
       mu_s <- rnorm(n = 10,
                     mean = MCMCsamples[iEta,'alpha'],
                     sd = MCMCsamples[iEta,'tau'])
-      
+
       # draw realizations of the physical parameter
       for(iPhi in 1:10){
-        samp_theta_pred[100*(iEta-1)+10*(iPhi-1)+(1:10)] <- 
+        samp_theta_pred[100*(iEta-1)+10*(iPhi-1)+(1:10)] <-
           rnorm(n = 10,
                 mean = mu_s[iPhi],
                 sd = MCMCsamples[iEta,'sigma'])
       }
-      
+
     }
-    
+
   }
-  
+
   # limit estimation to region specified by bounds on theta
   idx <- which(samp_theta_pred <= min(theta) | samp_theta_pred >= max(theta))
   samp_theta_pred[idx] <- NA
-  
+
   # calculate non parametric density
   density_theta <- density(samp_theta_pred,na.rm = T)
-  
+
   # interpolate density on desired points theta given in argument
   d_theta_pred = approx(x = density_theta$x,
                         y = density_theta$y,
                         xout = theta,yleft=0,yright=0)
-  
+
   ###################################################
   # Calculate uninformative distribution for theta  #
   ###################################################
-  
+
   # For comparison with informative case
-  
+
   samp_theta_prior = numeric(length = 1000*nrow(MCMCsamples))
-  
+
   # here multiplied by 10 because it is harder to reach convergence
-  for(iEta in 1:(10*nrow(MCMCsamples))){ 
-    
+  for(iEta in 1:(10*nrow(MCMCsamples))){
+
     if(hierarchicalSigma){
-      
+
       #sample eta from prior distribution
       alpha <- runif(1,min = -10,max = 10)
       tau <- runif(1,min = 0, max = 2)
       beta <- runif(1,min = 0, max = 5)
       xi <- runif(1,min = 0, max = 2)
-      
+
       # sample site-specific parameters
       mu_s = rnorm(n = 10,
                    mean = alpha,
@@ -316,54 +316,54 @@ generalFromMeas <- function(meas,
                             shape = beta,
                             rate = xi)
       sigma2_s <- 1/inv_sigma2_s
-      
+
       # sample theta parameters
       for(iPhi in 1:10){
         samp_theta_prior[100*(iEta-1)+10*(iPhi-1)+(1:10)] <-
           rnorm(n = 10,
                 mean = mu_s[iPhi],
-                sd = sqrt(sigma2_s[iPhi]))  
+                sd = sqrt(sigma2_s[iPhi]))
       }
-      
+
     }else{
-      
+
       # sample eta from prior distribution
       alpha <- rnorm(n = 1, mean = 0,sd = 1000) # approximates flat prior
       tau <- rcauchy(n = 1, location = 0,scale = 25) # half-Cauchy prior
       if(tau < 0 ) {tau <- (-tau)}
-      
+
       # sample site-specific parameters
       mu_s <- rnorm(n = 10, mean = alpha, sd = tau)
       sigma <- rgamma(n = 10, shape = 0.0001, rate = 0.0001) # approximates Jeffrey's prior (inverse prior)
-      
+
       # sample theta parameters
       for(iPhi in 1:10){
         samp_theta_prior[100*(iEta-1)+10*(iPhi-1)+(1:10)] <-
           rnorm(n = 10,
                 mean = mu_s[iPhi],
-                sd = sigma[iPhi])  
+                sd = sigma[iPhi])
       }
-      
+
     }
-    
+
   }
-  
+
   # limit estimation to region specified by bounds on theta
   idx <- which(samp_theta_prior <= min(theta) | samp_theta_prior >= max(theta))
   samp_theta_prior[idx] <- NA
-  
+
   # calculate non parametric density
   density_theta <- density(samp_theta_prior,na.rm = T)
-  
+
   # interpolate density on desired points theta given in argument
   d_theta_prior = approx(x = density_theta$x,
                          y = density_theta$y,
                          xout = theta,yleft=0,yright=0)
-  
+
   ####################
   ## return results ##
   ####################
-  
+
   return(list(gPrior=d_theta_pred, # regionalized prior for theta
               uPrior=d_theta_prior, # uninformative prior for theta
               hyperPar=hyperPar, # list of hyperparameters
@@ -372,6 +372,6 @@ generalFromMeas <- function(meas,
               meas=meas,
               MCMC=list(MCMCsamples=MCMCsamples,
                         MCMC_effectiveSizes=MCMC_effectiveSizes)))
-  
+
 }
 
