9dba7de8246efba42423a41fc2678ded4f994255
kcucchi <karina.cucchi@gmail.com>
2017-03-16 15:47:54 -0700

    add script for functions related to turning measurements in database to dataframes

---
+++ R/wwhypda2df_MySQL.R
@@ -0,0 +1,15 @@
+## functionName ##
+
+#'query measurements from wwhypda database
+#'
+#'\code{functionName} does blabla
+#'
+#'@param par1 what is par1
+#'@param par2 what is par2
+#'@return a dataframe blabla
+#'@examples
+#'write examples here
+#'@export # this indicates that the function will be available to users
+generalFromMeas <- function(meas){
+  # write function here
+}
