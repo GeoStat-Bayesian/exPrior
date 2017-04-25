7dd9247cfec1b3309caf0627882ee01d47b974b2
kcucchi <karina.cucchi@gmail.com>
2017-04-25 16:24:21 -0700

    add plot_meas function

---
+++ R/plot_meas.R
@@ -0,0 +1,20 @@
+#'plot histogram of measurements
+#'
+#'\code{plot_meas} plots histogram measurements as provided to generalFromMeas
+#'
+#'@param meas a dataframe containing measurements, as provided to the function generalFromMeas
+#'@param bindwidth a numeric specifying the width of the bins (optional)
+#'@return a plot
+#'@export
+plot_meas <- function(meas,bindwidth=NULL){
+  ggplot(meas, aes(x=val, fill = site_id))+
+    geom_histogram(binwidth = bindwidth)+
+    scale_fill_discrete("Site") +
+    labs(x = expression(theta), y='Counts') +
+    theme( axis.text.y = element_text(colour="#00A4E6", size=14),
+           axis.text.x = element_text(size = 13),
+           text = element_text(),
+           legend.background = element_rect(colour = "black"))
+}
+
+
