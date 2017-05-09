3a60081b3a184736c3f1f1db3d2b9def61a3e69b
kcucchi <karina.cucchi@gmail.com>
2017-05-09 14:58:44 -0700

    add option for tuning xrange in histogram

--- R/plot_meas.R
+++ R/plot_meas.R
@@ -4,10 +4,16 @@
 #'
 #'@param meas a dataframe containing measurements, as provided to the function generalFromMeas
 #'@param bindwidth a numeric specifying the width of the bins (optional)
+#'@param showLegend a boolean indicating whether to show the legend
+#'with the names of sites (optional, defaults to true)
+#'@param xrange a vector with the limits for the x-axis site
 #'@return a plot
 #'@export
-plot_meas <- function(meas,bindwidth=NULL,showLegend=T){
-  gPlot <- 
+plot_meas <- function(meas,
+                      bindwidth=NULL,
+                      xrange=NULL,
+                      showLegend=T){
+  gPlot <-
     ggplot(meas, aes(x=val, fill = site_id))+
     geom_histogram(binwidth = bindwidth)+
     scale_fill_discrete("Site") +
@@ -16,12 +22,15 @@ plot_meas <- function(meas,bindwidth=NULL,showLegend=T){
            axis.text.x = element_text(size = 13),
            text = element_text(),
            legend.background = element_rect(colour = "black"))
-  if(showLegend){
-    gPlot
-  }else{
-    gPlot + theme(legend.position="none")
+
+  if(!(is.null(xrange))){
+    gPlot <- gPlot +  scale_x_continuous(limits = xrange)
   }
-    
+
+  if(!showLegend){gPlot + theme(legend.position="none")}
+
+  gPlot
+
 }
 
 
