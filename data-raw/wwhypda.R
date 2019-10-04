1c2fd0e5254e4a1e70a513d81f4eda7cab912193
fhesze <falk.hesze@gmail.com>
2019-10-04 20:27:45 +0200

    removed all data files

--- data-raw/wwhypda.R
+++
@@ -1,8 +0,0 @@
-
-
-# extracts data from wwhypda
-
-df_wwhypda_K <-
-  rPrior::getData(param = "hydraulic conductivity",
-                   password = '<password>',db_name = 'db_wwhypda')
-devtools::use_data(df_wwhypda_K)
