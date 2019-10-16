6ba44ffb601ae3048913f13a37cd2927366c03d5
fhesze <falk.hesze@gmail.com>
2019-10-16 21:14:37 +0200

    added the porosity values from WWHYPDA to exPrior as an example dataset

---
+++ R/df_porosity.R
@@ -0,0 +1,12 @@
+#' 566 porosity values
+#'
+#' A dataset containing 566 porosity values from 6 different sites
+#'
+#' @format A data frame with 566 rows and 2 variables:
+#' \describe{
+#'   \item{site_id}{id of the site, categorical}
+#'   \item{val}{porosity value, non-dimensional fraction}
+#'   ...
+#' }
+#' @source \url{https://github.com/GeoStat-Bayesian/geostatDB}
+"df_porosity"
